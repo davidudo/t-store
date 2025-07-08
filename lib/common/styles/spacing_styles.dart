18a0b6190cb1f06f643ea5ef0cf24153d8b310b6
David Udo <65337929+davidudo@users.noreply.github.com>
2025-07-08 08:47:45 +0100

    feat(frontend): create verify email screens

--- lib/common/styles/spacing_styles.dart
+++ lib/common/styles/spacing_styles.dart
@@ -3,7 +3,7 @@ import 'package:t_store/utils/constants/sizes.dart';
 
 class TSpacingStyle {
   static const EdgeInsetsGeometry paddingWithAppBarHeight = EdgeInsets.only(
-    top: TSizes.appBarHeight,
+    top: TSizes.appBarHeight * 2,
     bottom: TSizes.defaultSpace,
     left: TSizes.defaultSpace,
     right: TSizes.defaultSpace,
