e31e5ed85e71886224c6fa11ecccdbc4d953b122
David Udo <65337929+davidudo@users.noreply.github.com>
2025-07-08 09:51:05 +0100

    feat(frontend): create forget password screens

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:t_store/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:t_store/features/authentication/screens/signup/signup.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
@@ -52,7 +53,7 @@ class TLoginForm extends StatelessWidget {
 
                 /// Forget Password
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () => Get.to(() => ForgetPasswordScreen()),
                   child: const Text(TTexts.forgetPassword),
                 ),
               ],
--- lib/features/authentication/screens/password_configuration/reset_password.dart
+++ lib/features/authentication/screens/password_configuration/reset_password.dart
@@ -1,45 +1,47 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:t_store/common/styles/spacing_styles.dart';
+import 'package:get/get.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
 
-class SuccessScreen extends StatelessWidget {
-  const SuccessScreen({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.subTitle,
-    required this.onPressed,
-  });
-
-  final String image, title, subTitle;
-  final VoidCallback onPressed;
+class ResetPasswordScreen extends StatelessWidget {
+  const ResetPasswordScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () => Get.back(),
+            icon: const Icon(CupertinoIcons.clear),
+          )
+        ],
+      ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: TSpacingStyle.paddingWithAppBarHeight,
+          padding: EdgeInsets.all(TSizes.defaultSpace),
           child: Column(
             children: [
               /// Image
               Image(
-                image: AssetImage(image),
+                image: const AssetImage(TImages.deliveredEmailIllustration),
                 width: THelperFunctions.screenWidth() * 0.6,
               ),
               const SizedBox(height: TSizes.spaceBtwSections),
 
               /// Title & Subtitle
               Text(
-                title,
+                TTexts.changeYourPasswordTitle,
                 style: Theme.of(context).textTheme.headlineMedium,
                 textAlign: TextAlign.center,
               ),
               const SizedBox(height: TSizes.spaceBtwItems),
               Text(
-                subTitle,
+                TTexts.changeYourPasswordSubTitle,
                 style: Theme.of(context).textTheme.labelMedium,
                 textAlign: TextAlign.center,
               ),
@@ -49,8 +51,16 @@ class SuccessScreen extends StatelessWidget {
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
-                  onPressed: onPressed,
-                  child: const Text(TTexts.tContinue),
+                  onPressed: () {},
+                  child: const Text(TTexts.done),
+                ),
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(TTexts.resendEmail),
                 ),
               ),
             ],
