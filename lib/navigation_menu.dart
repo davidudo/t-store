28efc442030ca03deed4b107b0b4fbe0f1f190cb
davidudo <udodavid46.ud@gmail.com>
2025-11-22 12:17:10 +0100

    refactor: update nav menu item active style

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:t_store/features/shop/screens/home/home.dart';
 import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
 
 class NavigationMenu extends StatelessWidget {
@@ -21,16 +22,116 @@ class NavigationMenu extends StatelessWidget {
           selectedIndex: controller.selectedIndex.value,
           onDestinationSelected: (index) =>
               controller.selectedIndex.value = index,
-          indicatorColor: darkMode ? TColors.black : TColors.white,
+          indicatorColor: Colors.transparent,
+          indicatorShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          labelTextStyle: WidgetStateProperty.resolveWith<TextStyle?>(
+            (Set<WidgetState> states) {
+              if (states.contains(WidgetState.selected)) {
+                return TextStyle(
+                  color: TColors.primary,
+                  fontSize: TSizes.fontSizeSm * 0.8,
+                );
+              }
+              return TextStyle(
+                fontSize: TSizes.fontSizeSm * 0.8,
+                color: darkMode
+                    ? TColors.white.withValues(alpha: 0.5)
+                    : TColors.black.withValues(alpha: 0.5),
+              );
+            },
+          ),
           backgroundColor: darkMode
               ? TColors.white.withValues(alpha: 0.1)
               : TColors.black.withValues(alpha: 0.1),
-          destinations: const [
-            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
-            NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
-            NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
-            NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+          destinations: [
+            NavigationDestination(
+              icon: Padding(
+                padding: const EdgeInsets.only(bottom: 2),
+                child: Icon(
+                  Iconsax.home,
+                  color: controller.selectedIndex.value == 0
+                      ? TColors.primary
+                      : null,
+                ),
+              ),
+              label: 'Home',
+              selectedIcon: Padding(
+                padding: const EdgeInsets.only(bottom: 2),
+                child: Icon(
+                  Iconsax.home,
+                  color: controller.selectedIndex.value == 0
+                      ? TColors.primary
+                      : null,
+                ),
+              ),
+            ),
+            NavigationDestination(
+              icon: Padding(
+                padding: const EdgeInsets.only(bottom: 2),
+                child: Icon(
+                  Iconsax.shop,
+                  color: controller.selectedIndex.value == 1
+                      ? TColors.primary
+                      : null,
+                ),
+              ),
+              label: 'Store',
+              selectedIcon: Padding(
+                padding: const EdgeInsets.only(bottom: 2),
+                child: Icon(
+                  Iconsax.shop,
+                  color: controller.selectedIndex.value == 1
+                      ? TColors.primary
+                      : null,
+                ),
+              ),
+            ),
+            NavigationDestination(
+              icon: Padding(
+                padding: const EdgeInsets.only(bottom: 2),
+                child: Icon(
+                  Iconsax.heart,
+                  color: controller.selectedIndex.value == 2
+                      ? TColors.primary
+                      : null,
+                ),
+              ),
+              label: 'Wishlist',
+              selectedIcon: Padding(
+                padding: const EdgeInsets.only(bottom: 2),
+                child: Icon(
+                  Iconsax.heart,
+                  color: controller.selectedIndex.value == 2
+                      ? TColors.primary
+                      : null,
+                ),
+              ),
+            ),
+            NavigationDestination(
+              icon: Padding(
+                padding: const EdgeInsets.only(bottom: 2),
+                child: Icon(
+                  Iconsax.user,
+                  color: controller.selectedIndex.value == 3
+                      ? TColors.primary
+                      : null,
+                ),
+              ),
+              label: 'Profile',
+              selectedIcon: Padding(
+                padding: const EdgeInsets.only(bottom: 2),
+                child: Icon(
+                  Iconsax.user,
+                  color: controller.selectedIndex.value == 3
+                      ? TColors.primary
+                      : null,
+                ),
+              ),
+            ),
           ],
+          labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
         ),
       ),
       body: Obx(() => controller.screens[controller.selectedIndex.value]),
